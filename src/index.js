2f135d8d4b56caac7e3b64f3ed8c4d6a8802e113
Nancy Singh <68498879+Nancy611@users.noreply.github.com>
2020-09-10 00:27:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
+import "./public/styles.css";
 
 ReactDOM.render(<form> </form>, document.getElementById("root"));
